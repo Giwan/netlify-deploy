27e29514daa6399c9b6208c0edc89a57c9eacf3e
Giwan Persaud <giwan.persaud@gmail.com>
2022-01-25 20:29:03 +0100

    action component

---
+++ components/ActionComponent/ActionComponent.js
@@ -0,0 +1,20 @@
+import { actionContainer, apiButton, section } from "./ActionComponent.module.css";
+
+const ActionComponent = function () {
+    return (
+        <div className={actionContainer}>
+            <section className={section}>
+                <header>Automation</header>
+                <p>Use the API from your existing app. You’ll receive a JSON object back indicating if the site can be opened in an iFrame or not. </p>
+                <button className={apiButton}>API</button>
+            </section>
+            <section className={section}>
+                <header>Manual</header>
+                <p>The sandbox let’s you load a given site directly into an iFrame. See detailed header information why the give site is not blocked or not.</p>
+                <a href="/sandbox">Sandbox</a>
+            </section>
+        </div>
+    )
+}
+
+export default ActionComponent;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,38 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles, { customerValue } from "../styles/Home.module.css";
+import ActionComponent from "../components/ActionComponent/ActionComponent";
 
 export default function Home() {
-  return (
-    <div className={styles.homeContainer}>
-      <Head>
-        <title>allows iframe</title>
-        <meta name="description" content="allows iframe" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h6 className={styles.iconTitle}>iFrame Service</h6>
-        <h1 className={styles.title}>Will it open in an iFrame?</h1>
-        <article className={styles.article}>
-          <p>
-            Not every site can be placed in an iFrame! Web developers might indicate in their response headers the site should not render in an iFrame.
-          </p>
-          <p>
-            All modern browsers will adhere to this request. An error message will be shown.
-            Use this service to pre-check if the given url will open in an iFrame.
-          </p>
-          <p>
-            Verify if a given site can be opened in an iFrame or if it will be blocked. Call the API with the given URL and receive JSON object in return
-          </p>
-        </article>
-
-      </main>
-
-    </div>
-  )
+    return (
+        <div className={styles.homeContainer}>
+            <Head>
+                <title>
+                    Allows iframe - API to check if a given site can be opened
+                    in an iFrame
+                </title>
+                <meta
+                    name="description"
+                    content="Allows iframe - API to check if a given site can be opened in an iFrame"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <h6 className={styles.iconTitle}>iFrame Service</h6>
+                <h1 className={styles.title}>Will it open in an iFrame?</h1>
+                <article className={styles.article}>
+                    <p>
+                        Not every site will work in an iFrame! The site owner
+                        can turn iFrames off. Good browsers will comply and show
+                        an error message.
+                    </p>
+                    <p className={customerValue}>
+                        Use this service to <strong>pre-check</strong> if the
+                        given <strong>URL</strong> will open in an{" "}
+                        <strong>iFrame</strong>.
+                    </p>
+                </article>
+            </main>
+            <ActionComponent />
+        </div>
+    );
 }
