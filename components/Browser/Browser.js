0a3aeafa9c208f46e857a04615b24805145081e5
Giwan Persaud <giwan.persaud@gmail.com>
2022-02-15 20:13:46 +0100

    validate url input

--- components/Browser/Browser.js
+++ components/Browser/Browser.js
@@ -28,14 +28,22 @@ const Browser = function ({ setIframeUrl, iframeUrl: url = "" }) {
     return (
         <div className={browserContainer}>
             <form className={browserHeader} onSubmit={handleSubmit}>
+                
                 <button className={browserButton}>Reload</button>
                 <input
                     type="text"
-                    value={_url || url}
+                    value={_url ?? url}
                     onChange={handleChange}
                     className={addressBar}
+                    spellCheck={false}
+                    autoCorrect={false}
+                    required
+                    pattern=".+\..+"
                     placeholder="https://mytoori.com"
+                    title="enter a valid URL here to load it into the iframe below"
+                    list="defaultURLs"
                 />
+                <DataList />
                 <button className={browserButton}>Go</button>
             </form>
             <IframeComponent {...{ iframeUrl: url }} />
@@ -48,3 +56,14 @@ export default Browser;
 const IframeComponent = ({ iframeUrl }) => (
     <iframe src={iframeUrl} className={iframe}></iframe>
 );
+
+const DataList = () => (
+    <datalist id="defaultURLs">
+        <option value="https://mytoori.com/" />
+        <option value="https://developer.mozilla.org/" />
+        <option value="https://phys.org/" />
+        <option value="http://www.bbc.co.uk" />
+        <option value="http://web.dev" />
+        <option value="http://w3.org/" />
+    </datalist>
+)
